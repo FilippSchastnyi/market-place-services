1dabc34b8b6d6c3949ab5ed30509fcb700d8f689
Pilip Shchasny <pilip.shchasny@fathom.io>
2024-10-01 22:18:34 +0200

    inventory-service: setup project and db

---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory_items.sql
@@ -0,0 +1,8 @@
+INSERT INTO t_inventory(quantity, sku_code)
+VALUES (100, 'IPHONE_16_PRO_MAX'),
+       (200, 'IPHONE_16_PRO'),
+       (300, 'IPHONE_15'),
+       (400, 'IPHONE_15_PRO_MAX'),
+       (500, 'IPHONE_15_PRO'),
+       (600, 'IPHONE_13');
+
